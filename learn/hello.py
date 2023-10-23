7b411c42b1c6dbeedfb28c479f4336464ad98bc3
Derek Gusoff <dgusoff@rightpoint.com>
2023-10-23 10:19:23 -0400

    initial

---
+++ learn/hello.py
@@ -0,0 +1,19 @@
+
+my_list = [1,2,3,4]
+print(my_list)
+my_list.append(5)
+
+# set
+my_set = {1,2,3,4,6}
+print(len(my_set))
+
+
+#tuple
+my_tuple = (1,2,3)
+len(my_tuple)
+
+#dictionary
+my_dictionary = {
+    "foo": "bar"
+}
+my_dictionary["foo"]
---
+++ main.py
@@ -0,0 +1,17 @@
+import wave
+import time
+import sys
+
+import pyaudio
+
+p = pyaudio.PyAudio()
+print('hey')
+
+info = p.get_device_count()
+#info = p.get_default_output_device_info()
+print(info)
+
+for x in range(0, info):
+    #print("We're on time %d" % (x))
+    data = p.get_device_info_by_index(x)
+    print(f"{x} - {data['defaultSampleRate']} - {data['name']} - {data['hostApi']}")
